3ab8219519bf5dae7e5a2c4e974e6071ddfb6f76
Daric Zhou <dariczhou@gmail.com>
2019-01-27 21:00:43 -0800

    Old Frac calc

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -29,22 +29,19 @@ public class FracCalc {
 	   public static String produceAnswer(String input) { 
    		   
 		   String[] cutExpression = input.split(" ");
-		   
-		   //Creates arrays of multiple operands and operators
-		   String[] operands = new String[round(cutExpression.length/2 + 0.5)+1];
-		   String[] operators = new String[round(cutExpression.length/2 + 0.5)];
-		   
+		 //Creates two new arrays to contain the operators and the operands
+			String[] operands = new String[round(cutExpression.length/2.0 + 0.5)];
+			String[] operators = new String[round(cutExpression.length/2.0 - 0.5)];
 		   //Valid expressions for the scanner to take in
-		   char[] allowedOperands = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '/'};
+		   char[] allowedOperands = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '/', '-'};
 		   char[] allowedOperators = {'+', '-', '*', '/'}; 
-		   
+
 		   String result = null;
-			for(int i = 0; i < cutExpression.length; i++) {
+			for(int i = 0; i < cutExpression.length;  i ++) {
 				if(i % 2 == 0) {
-					if(checkIfValid(cutExpression[i], allowedOperands)) { 
-						operator(cutExpression[i+1], toRealFrac(cutExpression[i]));
-						result = ;
-					}	
+					if(checkIfValid(cutExpression[i], allowedOperands)) { 		
+						operands[i/2] = cutExpression[i];
+					}
 					else {
 						return "ERROR: Input is in an invalid format";
 					}
@@ -52,14 +49,19 @@ public class FracCalc {
 				else {
 					if(checkIfValid(cutExpression[i], allowedOperators) && cutExpression[i].split("").length < 2) {
 						operators[i/2] = cutExpression[i];
+					
+					//If any are invalid, return an error immediately
 					}
-					else {
-						return "ERROR: \"" + cutExpression[i] + "\" is not a valid format" ;
+					if(checkIfValid(cutExpression[i], allowedOperators) && cutExpression[i].split("").length < 2) {
+						return "ERROR: \"" + cutExpression[i+1] + "\" is not a valid format" ;
 					}
 				}
+				result = toReducedFrac(operator(operators[i/2], toRealFrac(cutExpression[i]), toRealFrac(cutExpression[2])));
 			}
+			return result;
 		
 	   }
+	   //parses the string array into integers
 		public static int[] toRealFrac(String operand) {
 			int[] intFrac = {0, 0, 1};
 				if(operand.contains("_")) {
@@ -79,7 +81,7 @@ public class FracCalc {
 				}
 				return intFrac;
 		}
-		//returns the improper fraction of a fraction if it's mixed
+		//turns into an improper fraction as an array
 		public static void toImproper(int[] fraction) {
 			fraction[1] = fraction[1] + fraction[2] * Math.abs(fraction[0]);
 			if(fraction[0] < 0) { //deals with negative fractions
@@ -88,16 +90,37 @@ public class FracCalc {
 			fraction[0] = 0;
 		}
 		//This method executes the expression entered through the operators and operand
-			public static int[] operator(String operator, int[] operand) {
+			public static int[] operator(String operator, int[] operand, int[] nextoperand) {
+				int[] answer = new int[3];
 				toImproper(operand);
+				toImproper(nextoperand);
+				answer[2] = (operand[2]*nextoperand[2]); //set common denominator
 				if(operator.equals("+")) {
-					
+					answer[1] = (operand[1]*nextoperand[2]) + (nextoperand[1]*operand[2]);
+					}
+				if(operator.equals("-")) {
+					answer[1] = (operand[1]*nextoperand[2]) - (nextoperand[1]*operand[2]);
+					}
+				if(operator.equals("/")) {
+					answer[1] = (operand[1]*nextoperand[2]);
+					answer[2] = (operand[2]*nextoperand[1]);
+					}
+				else if(operator.equals("*")) {
+					answer[1] = (operand[1]*nextoperand[1]);
+					}
+				//changes the sign to be in front of the final value
+				if(answer[2] < 0) {
+					answer[1] = -answer[1];
+					answer[2] = Math.abs(answer[2]);
 				}
+				return answer;
 			}
 
 				//after going through the operators, this method simplifies the result produced by the operator methods
 			public static String toReducedFrac(int[] result) {
-
+				if(result[2] == 0) {
+					return "ERROR: Cannot divide by 0";
+				}
 					int gcf = gcf(Math.abs(result[1]),Math.abs(result[2])); 
 
 					if(gcf >= 1 && Math.abs(result[1])%Math.abs(result[2]) != 0 && Math.abs(result[1]) > Math.abs(result[2])) {
@@ -136,9 +159,6 @@ public class FracCalc {
 				return wholenum + "_" + Math.abs(newnum) + "/" + Math.abs(denom);
 			} 
 		
-	}
-
-
 
 	   //This function checks if the operator entered is valid based off the given String array(EC)
 		public static boolean checkIfValid(String check, char[] expression) {
@@ -164,9 +184,11 @@ public class FracCalc {
 		public static int round(double n) {
 			return (int)(n +  0.5);
 		}
+		//takes an improper fraction array and changes it to a mixed num
+		public static int[] toOriginal(int[] improperFrac) {
+			int num = improperFrac[1] % improperFrac[2];
+			improperFrac[0] = (improperFrac[1] - num) / improperFrac[2];
+			improperFrac[1] = num;
+			return improperFrac;
+		}
 }
-		
-
-
-		
-		
